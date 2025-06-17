a5fcb74b497e6b7ad82cc899810850ca75f08b1c
Alberto Occelli <a.occelli@reply.it>
2025-06-17 20:33:14 +0200

    Change language from English to Italian in text-to-speech function and update output file name to "oggi.mp3".

--- tts.py
+++ tts.py
@@ -3,9 +3,9 @@ from gtts import gTTS
 
 def text_to_speech(input_text):
     # Convert the input text into speech
-    speech = gTTS(text = input_text, lang = 'en', slow = False)
+    speech = gTTS(text = input_text, lang = 'it', slow = False)
     
     # Save the speech audio into a file
-    speech.save("today.mp3")
+    speech.save("oggi.mp3")
 
 text_to_speech("Accadde oggi")

d66191d609bb72bd8ab0e21c9c6cfd182bdda7c4
Alberto Occelli <a.occelli@reply.it>
2025-09-10 11:09:24 +0200

    Enhance logging configuration for improved message formatting

--- gdrive_daemon.py
+++ gdrive_daemon.py
@@ -2,6 +2,10 @@ import logging
 import subprocess
 import time
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+)
 log = logging.getLogger(__name__)
 
 while True:
